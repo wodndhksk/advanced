09cbef483decd2de2b9a63edea956b4f6ad53224
Jaewoo Kim <wodndhksk@gmail.com>
2023-12-16 17:09:57 +0900

    쓰레드 로컬 동기화 - 개발

---
+++ src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,43 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> service.logic("userA");
+        Runnable userB = () -> service.logic("userB");
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000); // 동시성 문제 x
+        sleep(100); // 0.1 초로 수정 -> 동시성 문제 발생 (threadA 와 threadB에서 "userB"를 조회하는 오류)
+        threadB.start();
+
+        sleep(3000);
+        log.info("main end");
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
