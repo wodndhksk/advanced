0dc82054475da4407a96c758e4ab5dc55c40052c
Jaewoo Kim <wodndhksk@gmail.com>
2023-12-18 10:45:17 +0900

    전략 패턴 - 예제1
    - 인터페이스의 위임을 사용하여 구현 (상속의 단점을 interface 의 위임으로 극복)

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,9 @@
-package hello.advanced.trace;
+package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -29,4 +33,16 @@ public class ContextV1Test {
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
     }
+
+    @Test
+    void strategyV1() {
+        Strategy strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        Strategy strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+    }
+
 }
