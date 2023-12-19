cfa8e9f48ced84df78fd7b81ab99b2eda67a8aea
Jaewoo Kim <wodndhksk@gmail.com>
2023-12-19 10:37:02 +0900

    전략 패턴 - 예제2 - 전략 패턴 또한 익명 내부 클래스로 구현이 가능하다. - 람다로 변경하려면 인터페이스에 메서드가 1개만 있어야 한다.

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -45,4 +45,64 @@ public class ContextV1Test {
         contextV2.execute();
     }
 
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        log.info("strategyLogic1={}", strategyLogic1);
+        context1.execute();
+
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        log.info("strategyLogic2={}", strategyLogic2);
+        context2.execute();
+    }
+
+    /**
+     *  변수로 선언하지 않고 ContextV1에 바로 구현
+     */
+    @Test
+    void strategyV3() {
+        ContextV1 context1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        context2.execute();
+    }
+
+    /**
+     * 람다 사용
+     * (람다로 변경하려면 인터페이스에 메서드가 1개만 있으면 되는데,
+     *  여기에서 제공하는 Strategy 인터페이스는 메서드가 1개만 있으므로 람다로 사용할 수 있다)
+     */
+    @Test
+    void strategyV4() {
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        context2.execute();
+    }
+
 }
